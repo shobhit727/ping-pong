14f36b9f9983853bda7b0c021ca26230c6c1cdcb
Shobhit saraogi <79454607+shobhit727@users.noreply.github.com>
2021-03-25 16:51:32 +0530

    Add files via upload

---
+++ pong_two _player.py
@@ -0,0 +1,98 @@
+import pygame, sys
+
+def ball_animation():
+        global ball_speed_y , ball_speed_x
+        ball.x += ball_speed_x
+        ball.y += ball_speed_y
+
+        if ball.top <= 0 or ball.bottom >= screen_height:
+                ball_speed_y*=-1
+
+        if ball.left <= 0 or ball.right >= screen_width:
+                ball_speed_x*=-1
+
+        if ball.colliderect(player) or ball.colliderect(opponent):
+                ball_speed_x*=-1
+
+def player_animation():
+	player.y += player_speed
+
+	if player.top <= 0 :
+		player.top = 0
+	if player.bottom >= screen_height:
+		player.bottom = screen_height
+
+def opponent_animation():
+	opponent.y += opponent_speed
+	if opponent.top <= 0 :
+		opponent.top = 0
+	if opponent.bottom >= screen_height:
+		opponent.bottom = screen_height
+
+
+# General setup
+pygame.init()
+clock = pygame.time.Clock()
+
+# Setting up the main window
+screen_width = 800
+screen_height = 600
+screen = pygame.display.set_mode((screen_width,screen_height))
+pygame.display.set_caption('Pong')
+
+light_grey = (200, 200, 200)
+bg_color = pygame.Color('grey14')
+
+ball = pygame.Rect(screen_width / 2 - 15, screen_height / 2 - 15, 30, 30)
+
+player = pygame.Rect(screen_width - 20, screen_height / 2 - 70, 10,140)
+opponent = pygame.Rect(10, screen_height / 2 - 70, 10,140)
+
+ball_speed_x = 7
+ball_speed_y = 7
+
+player_speed = 0
+opponent_speed = 0
+
+while True:
+	#Handling input
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			sys.exit()
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_UP:
+				player_speed -= 7
+			if event.key == pygame.K_DOWN:
+				player_speed += 7
+
+			if event.key == pygame.K_w:
+				opponent_speed -= 7
+			if event.key == pygame.K_s:
+				opponent_speed += 7
+
+		if event.type == pygame.KEYUP:
+			if event.key == pygame.K_UP:
+				player_speed += 7
+			if event.key == pygame.K_DOWN:
+				player_speed -= 7
+
+			if event.key == pygame.K_w:
+				opponent_speed += 7
+			if event.key == pygame.K_s:
+				opponent_speed -= 7
+
+	ball_animation()
+	player_animation()
+	opponent_animation()
+
+	screen.fill(bg_color)
+	pygame.draw.ellipse(screen, light_grey, ball)
+	pygame.draw.rect(screen, light_grey, player)
+	pygame.draw.rect(screen, light_grey, opponent)
+
+	pygame.draw.aaline(screen, light_grey, (screen_width / 2, 0),(screen_width / 2, screen_height))
+	
+	# Updating the window
+	pygame.display.flip()
+	clock.tick(60)
